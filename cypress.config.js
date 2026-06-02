f96130aea3c81bbd34744d29fb9d34a9e07af66f
andrevlopes <andrellopes021@gmail.com>
2026-06-02 10:48:13 -0300

    fix: cypress config port

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:4173',
+    baseUrl: 'http://127.0.0.1:8080',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: false,
   },
